277bda24aa1713bb4d95fdb576da15f3ed6ff9fb
Karl Heitmann <khf1988@gmail.com>
2021-05-08 15:18:38 -0400

    listo el ejemplo para cambiar los select

--- app/javascript/packs/cambiar_seleccion.js
+++ app/javascript/packs/cambiar_seleccion.js
@@ -1,7 +1,12 @@
 const selectConfiguracion = document.querySelector('#listado-de-opciones')
 
-function cambiarConfiguracion() {
+function cambiarConfiguracion(e) {
   console.log("cambiarConfiguracion")
+  console.log("evento", e)
+  console.log(e.target.value); // En la variable e.target.value se ve cual fue el select seleccionado
+
+  const selectCountries = document.querySelector('#region_country_id')
+  selectCountries.innerHTML = `<option value='1'>EStO FUE CAMBIADO por ${e.target.value}</option>`
 }
 
 selectConfiguracion.addEventListener('change', cambiarConfiguracion)
