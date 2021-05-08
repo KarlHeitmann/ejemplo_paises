9e6dc9aabac478cdd5b9b3bf2a6f90583cafdb35
Karl Heitmann <khf1988@gmail.com>
2021-05-08 15:06:34 -0400

    jugando con select: agregado select y se le amarro un evento simple de change

---
+++ app/javascript/packs/cambiar_seleccion.js
@@ -0,0 +1,7 @@
+const selectConfiguracion = document.querySelector('#listado-de-opciones')
+
+function cambiarConfiguracion() {
+  console.log("cambiarConfiguracion")
+}
+
+selectConfiguracion.addEventListener('change', cambiarConfiguracion)
